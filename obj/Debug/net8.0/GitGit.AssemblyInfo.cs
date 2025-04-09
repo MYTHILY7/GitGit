326ff0d151220bc67ef4690b1e301fbc5e32a27d
mythily7 <mythilytm@gmail.com>
2025-04-09 16:57:08 +0530

    Committing 2nd time

--- obj/Debug/net8.0/GitGit.AssemblyInfo.cs
+++ obj/Debug/net8.0/GitGit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitGit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f97bd0025495dbcd6317ba1bde9004096d686d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitGit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitGit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
